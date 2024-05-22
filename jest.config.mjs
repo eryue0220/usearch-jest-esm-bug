5783511ff6f57a94c8b7efe145bbc8da0e3d4d8b
eryue0220 <eryue0220@gmail.com>
2024-05-22 14:46:49 +0800

    feat: init

--- jest.config.mjs
+++ jest.config.mjs
@@ -5,7 +5,6 @@ export default {
     'node_modules/.pnpm/usearch@2.11.1/node_modules/usearch/javascript/dist/esm/usearch.js': '<rootDir>/usearch-transform.js',
   },
   transformIgnorePatterns: [
-    '!node_modules/.pnpm/usearch@2.11.1',
     '\\.pnp\\.[^\\/]+$',
   ],
 };
